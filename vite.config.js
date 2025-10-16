0ba2c022af9f537c0619b0bfc2987996238f22f3
Tania S. Sainz de Aja <tssainzdeaja@gmail.com>
2025-10-16 18:53:09 +0200

    Preparing a project for deployment on Git-Hub

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [react()],
+  base: "/modulo-3-evaluacion-final-Tasac-sainz/",
   server: {
     open: "/",
     watch: {
